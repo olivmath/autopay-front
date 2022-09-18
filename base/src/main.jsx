e7be00c8d43c3d1d3ff5d6e6e67c34c2909d9d50
Lucas Oliveira <olivmath@protonmail.com>
2022-09-18 00:11:13 -0300

    init commit

---
+++ base/src/main.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './index.css'
+
+import App from "./components/header/App"
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
