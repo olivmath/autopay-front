b74698c1e925c17ac2b7ddc508a00fefe6159c32
Lucas Oliveira <olivmath@protonmail.com>
2022-09-18 00:22:37 -0300

    update: dir

--- base/src/services/CallCounter.js
+++
@@ -1,8 +0,0 @@
-import abi from "./contractABI.json"
-import { ethers } from "ethers";
-
-const contractAddress = "0x180ef25d4E5ABd84a0545baE3e847A17C0e012d0" // Goerli
-const provider = new ethers.providers.Web3Provider(window.ethereum)
-const contractABI = abi.abi
-
-export const callCounterContract = new ethers.Contract(contractAddress, contractABI, provider.getSigner());
